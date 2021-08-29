cc8dd97effc9bf87ae96cacf944206ea6e53963a
Joy <abcdef@gmail.com>
2021-08-29 11:26:32 +0800

    First commit

---
+++ radius.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout<<"Please enter a radius: ";
+int radius;
+cin>>radius;
+float PI=3.14159;
+cout>>"The area of this circle= "<<(PI*radius*radius);
+return 0;
+}
